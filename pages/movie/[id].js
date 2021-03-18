84800f7ccc69aa5b2e606852ca490a584cc42739
BrenerViotto <brenerviotto@gmail.com>
2021-03-18 14:28:12 -0300

    Update [id].js

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -28,7 +28,7 @@ export default function MovieItem({info}) {
 }
 
 export async function getServerSideProps(context) {
-  const res = await fetch(`http://localhost:3000/api/movie/${context.params.id}`);
+  const res = await fetch(`https://filmes.brener.ml/api/movie/${context.params.id}`);
   const json = await res.json();
 
   return {
